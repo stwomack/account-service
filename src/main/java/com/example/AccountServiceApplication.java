2fe76cd2c5bb7027e7ef55285c4cd0ec07747406
Steve Womack <stwomack@gmail.com>
2016-01-31 14:44:14 -0600

    Adds type to accounts

--- src/main/java/com/example/AccountServiceApplication.java
+++ src/main/java/com/example/AccountServiceApplication.java
@@ -3,16 +3,24 @@ package com.example;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.integration.annotation.InboundChannelAdapter;
+import org.springframework.integration.core.MessageSource;
+import org.springframework.messaging.support.GenericMessage;
 
+import java.text.SimpleDateFormat;
 import java.util.Collection;
+import java.util.Date;
 import java.util.stream.Stream;
 
 @SpringBootApplication
+@EnableBinding
 public class AccountServiceApplication {
 
 	public static void main(String[] args) {
