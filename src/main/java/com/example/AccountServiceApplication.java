f51956d47455665f3b7e0a92cdc73fe553dd8b0b
Steve Womack <stwomack@gmail.com>
2016-01-31 22:46:48 -0600

    Adds my config server

--- src/main/java/com/example/AccountServiceApplication.java
+++ src/main/java/com/example/AccountServiceApplication.java
@@ -3,11 +3,13 @@ package com.example;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -25,6 +27,8 @@ import java.util.stream.Stream;
 
 @SpringBootApplication
 @EnableBinding
+@Configuration
+@EnableAutoConfiguration
 public class AccountServiceApplication {
 
 	public static void main(String[] args) {
