4d34f3c404bc62afe451408cbaa427acb9230876
Steve Womack <stwomack@gmail.com>
2016-05-12 15:00:00 -0500

    Duh

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -6,5 +6,5 @@ pushd source-code
   ./mvnw clean package
 popd
 
-git clone ./input ./output
+git clone ./source-code ./build-output
 #cp source-code/target/account-service-0.0.1-SNAPSHOT.jar build-output/.
