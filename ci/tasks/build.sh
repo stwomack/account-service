6a5fef79161bebc3a8671bb89cf8b68ec0cc8a1b
Steve Womack <stwomack@gmail.com>
2016-05-12 14:53:21 -0500

    Updates

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -6,4 +6,5 @@ pushd source-code
   ./mvnw clean package
 popd
 
-cp source-code/target/account-service-0.0.1-SNAPSHOT.jar build-output/.
+git clone ./input ./output
+#cp source-code/target/account-service-0.0.1-SNAPSHOT.jar build-output/.
