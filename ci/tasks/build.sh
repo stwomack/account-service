3da500bd428ba4348ebaf18956aa85b61eb8545e
Steve Womack <stwomack@gmail.com>
2016-05-12 18:01:02 -0500

    Removes 'drop' from ddl-auto

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -6,5 +6,4 @@ pushd source-code
   ./mvnw clean package
 popd
 
-git clone ./source-code ./build-output
-#cp source-code/target/account-service-0.0.1-SNAPSHOT.jar build-output/.
+cp source-code/target/account-service-0.0.1-SNAPSHOT.jar build-output/.
