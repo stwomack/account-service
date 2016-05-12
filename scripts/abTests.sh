6b5ee0e1fa8aea7eac53da65fa664052326c1bfc
Steve Womack <stwomack@gmail.com>
2016-05-12 16:17:47 -0500

    Removes 'drop' from ddl-auto

--- scripts/abTests.sh
+++ scripts/abTests.sh
@@ -2,4 +2,4 @@
 
 set -e -x
 
-ab -rl -c 100 -n 1000 -p account.json -v 0 -T 'application/json' "https://accountz.pcf1.fe.gopivotal.com/accounts"
+ab -r -l -c 400 -n 10000  -p account.json -v 0 -T 'application/json' "https://accountz.pcf1.fe.gopivotal.com/accounts"
