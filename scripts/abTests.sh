072aeec6a299ef72f04e40651a4b6ee963a0c514
Steve Womack <stwomack@gmail.com>
2016-05-14 18:06:40 -0500

    Add config server & eureka

--- scripts/abTests.sh
+++ scripts/abTests.sh
@@ -2,4 +2,4 @@
 
 set -e -x
 
-ab -r -l -c 400 -n 10000  -p account.json -v 0 -T 'application/json' "https://accountz.pcf1.fe.gopivotal.com/accounts"
+ab -r -l -c 5 -n 200  -p account.json -v 0 -T 'application/json' "https://accountz.pcf1.fe.gopivotal.com/accounts"
--- src/main/java/com/example/AccountServiceApplication.java
+++ src/main/java/com/example/AccountServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Source;
@@ -30,6 +31,7 @@ import java.util.stream.Stream;
 @EnableBinding
 @Configuration
 @EnableAutoConfiguration
+@EnableDiscoveryClient
 public class AccountServiceApplication {
 
 	public static void main(String[] args) {
@@ -52,15 +54,15 @@ interface AccountRepository extends PagingAndSortingRepository<Account, Long> {
 	Collection<Account> findByAccountName(@Param("accountName") String accountName);
 }
 
-//@RefreshScope
-//@RestController
-//class MessageRestController {
-//
-//	@Value("${foo}")
-//	private String message;
-//
-//	@RequestMapping("/foo")
-//	String getMessage() {
-//		return this.message;
-//	}
-//}
+@RefreshScope
+@RestController
+class MessageRestController {
+
+	@Value("${foo}")
+	private String message;
+
+	@RequestMapping("/foo")
+	String getMessage() {
+		return this.message;
+	}
+}
