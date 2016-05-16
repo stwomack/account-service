a7e35cb7d93a0d5238e97104a02872cf5abf4e61
Steve Womack <stwomack@gmail.com>
2016-05-16 16:04:21 -0500

    Gets ready for workshop

--- src/main/java/com/example/AccountServiceApplication.java
+++ src/main/java/com/example/AccountServiceApplication.java
@@ -7,31 +7,21 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
-import org.springframework.integration.annotation.InboundChannelAdapter;
-import org.springframework.integration.core.MessageSource;
-import org.springframework.messaging.support.GenericMessage;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.text.SimpleDateFormat;
 import java.util.Collection;
-import java.util.Date;
 import java.util.stream.Stream;
 
 @SpringBootApplication
-@EnableBinding
-@Configuration
-@EnableAutoConfiguration
-@EnableDiscoveryClient
+//@EnableAutoConfiguration
+//@EnableDiscoveryClient
 public class AccountServiceApplication {
 
 	public static void main(String[] args) {
