106b248165337a726d897bf19b580509c2e38e63
Steve Womack <stwomack@gmail.com>
2016-06-03 15:57:49 -0500

    Forgot

--- src/main/java/com/example/AccountServiceApplication.java
+++ src/main/java/com/example/AccountServiceApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -19,40 +18,44 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Collection;
 import java.util.stream.Stream;
 
-@SpringBootApplication
-//@EnableAutoConfiguration
-//@EnableDiscoveryClient
-public class AccountServiceApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(AccountServiceApplication.class, args);
-	}
-
-	@Bean
-	CommandLineRunner dummyCLR(AccountRepository reservationRepository) {
-		return args -> {
-			Stream.of("Womack", "Dostoyevsky", "Rockefeller", "Ghandi")
-					.forEach(name -> reservationRepository.save(new Account(name, "checking")));
-			reservationRepository.findAll().forEach(System.out::println);
-		};
-	}
-}
-
 @RepositoryRestResource
 interface AccountRepository extends PagingAndSortingRepository<Account, Long> {
-	@RestResource(path = "by-name")
-	Collection<Account> findByAccountName(@Param("accountName") String accountName);
+    @RestResource(path = "by-name")
+    Collection<Account> findByAccountName(@Param("accountName") String accountName);
+}
+
+@SpringBootApplication
+@EnableAutoConfiguration
+
+@EnableDiscoveryClient
+public class AccountServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountServiceApplication.class, args);
+    }
+
+
+    @Bean
+    CommandLineRunner dummyCLR(AccountRepository reservationRepository) {
+        return args -> {
+            Stream.of("Womack", "Dostoyevsky", "Rockefeller", "Ghandi")
+                    .forEach(name -> reservationRepository.save(new Account(name, "checking")));
+            reservationRepository.findAll().forEach(System.out::println);
+
+        };
+    }
+
 }
 
 @RefreshScope
 @RestController
 class MessageRestController {
 
-	@Value("${foo}")
-	private String message;
+    @Value("${foo}")
+    private String message;
 
-	@RequestMapping("/foo")
-	String getMessage() {
-		return this.message;
-	}
+    @RequestMapping("/foo")
+    String getMessage() {
+        return this.message;
+    }
 }
