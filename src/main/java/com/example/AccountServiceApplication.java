c919648b556be5f95d0f0ccfe7e3db5279e0d1a7
Steve Womack <stwomack@gmail.com>
2016-10-20 10:21:34 -0500

    yolo

--- src/main/java/com/example/AccountServiceApplication.java
+++ src/main/java/com/example/AccountServiceApplication.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -8,31 +9,43 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
 import java.util.Collection;
 import java.util.stream.Stream;
 
 @RepositoryRestResource
 interface AccountRepository extends PagingAndSortingRepository<Account, Long> {
+
     @RestResource(path = "by-name")
     Collection<Account> findByAccountName(@Param("accountName") String accountName);
+
 }
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @EnableDiscoveryClient
+//@EnableJpaRepositories // <---- Add this
+//@Import(RepositoryRestMvcConfiguration.class) // <---- And this
 public class AccountServiceApplication {
+
+    @Value("${myQuery}")
+    private String query;
 
     public static void main(String[] args) {
         SpringApplication.run(AccountServiceApplication.class, args);
     }
-
 
     @Bean
     CommandLineRunner dummyCLR(AccountRepository reservationRepository) {
@@ -43,6 +56,7 @@ public class AccountServiceApplication {
 
         };
     }
+
 }
 
 @RefreshScope
@@ -56,5 +70,14 @@ class MessageRestController {
     String getMessage() {
         System.out.println("MESSAGE CALLED");
         return this.message;
+    }
+
+    @HystrixCommand(fallbackMethod = "reliable")
+    public String getCircuit() {
+        return "This is succeeding";
+    }
+
+    public String reliable() {
+        return "This is faling";
     }
 }
