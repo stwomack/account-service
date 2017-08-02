f0ae995e884dd05147f96fd8d98be44d5d7d8540
Administrator <admin@example.com>
2017-08-02 11:27:25 -0500

    yolo

--- src/main/java/com/example/Account.java
+++ src/main/java/com/example/Account.java
@@ -1,7 +1,5 @@
 package com.example;
 
-import org.springframework.context.annotation.PropertySource;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
